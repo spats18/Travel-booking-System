a73394df217da6c10e710d18942b27fffe2aa7d8
Sannidhya Pathania <pathaniasannidhya@gmail.com>
2022-11-20 16:23:19 -0700

    dev

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from website import create_app # we can do this bec now our website is a package
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug = True) # debug = True will automatically rerun our website if we did any change in the code. 
+    app.run(debug = True) 
+    # debug = True will automatically rerun our website if we did any change in the code. 
     #will turn it off in production
--- website/__init__.py
+++ website/__init__.py
@@ -5,6 +5,12 @@ def create_app():
     app = Flask(__name__) # initialization of flask
     app.config['SECRET_KEY'] = 'random key to encrypt' # encrypt and secure the cookies and session data related to our website
 
+    from .views import views
+    from .auth import auth 
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/auth')
+
     return app
     # So above will create the Flask Application and initialize the secret key and we returned it through above function
 
